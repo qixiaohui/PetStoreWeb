56ec91da2f5296208a7653c85aba4d4b3b03f146
qixiaohui <qixiaohuihaha@gmail.com>
2016-12-18 13:47:43 -0800

    add post

--- assets/js/pages/base.jsx
+++ assets/js/pages/base.jsx
@@ -16,7 +16,34 @@ var base = React.createClass({
     render: function(){
         return(
             <div>
-                {this.props.children}
+                <div className="header">
+                  <div className="navbar navbar-default navbar-fixed-top" role="navigation">
+                    <div className="container">
+                      <div className="navbar-header">
+                        <form  className="navbar-form" style={{float: "left"}}>
+                          <button id="sidebarButton" type="button" className="btn btn-default">
+                            <span className="glyphicon glyphicon-list" aria-hidden="true"></span>
+                          </button>
+                        </form>
+                      </div>
+
+                      <div className="collapse navbar-collapse" id="js-navbar-collapse">
+                        <ul className="nav navbar-nav">
+                          <li className="active"><a href="#/"><b>Petstore</b></a></li>
+                        </ul>
+                        <form className="navbar-form navbar-right" style={{float: "right"}} role="search">
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Search topic" />
+                            </div>
+                            <button type="submit" className="btn btn-default"><span className="glyphicon glyphicon-search"></span></button>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="container">
+                    {this.props.children}
+                </div>
             </div>
         );
     }
--- assets/js/pages/posts/posts.jsx
+++ assets/js/pages/posts/posts.jsx
@@ -26,11 +26,19 @@ var posts = React.createClass({
     },
     render: function() {
         return (
-            <div>
+            <div className="marketing" style={{marginTop: "100px", paddingLeft: "15px", paddingRight: "15px"}}>
                 {_.map(this.state.posts, (post) => {
                     return (
-                        <div key={post.id}>
-                            <p>{post.content}</p>
+                        <div className="row" style={{marginBottom: "40px"}}>
+                            <div className="col-md-3" style={{marginTop: "10dp"}}>
+                                {post.image === null?null:<img className="img-responsive" src={"http://localhost:8000"+post.image} />}
+                            </div>
+                            <div className="col-md-9" href="new.link">
+                                <h4>{post.title}</h4>
+                                <p style={{color: "#5d5f60"}}>{post.content}</p>
+                                <p style={{float: "left", fontSize: "12px", color: "#a9abae"}} id="author">Post by: {post.user}</p>
+                                <p style={{float: "right", fontSize: "12px", color: "#848a91"}}>{post.timestamp}</p>
+                            </div>
                         </div>
                     );
                 })}
